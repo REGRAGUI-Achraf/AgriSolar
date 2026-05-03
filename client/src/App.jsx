ade5b4981f78e4be10cc8ea13717629e1fa86934
REGRAGUI-Achraf <154520796+REGRAGUI-Achraf@users.noreply.github.com>
2026-05-03 05:46:48 +0100

    try implementation

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import SizingPage from './pages/SizingPage';
+
+export default function App() {
+	return (
+		<div className="min-h-screen bg-slate-50">
+			<SizingPage />
+		</div>
+	);
+}
